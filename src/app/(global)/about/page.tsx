f6d4c4e5bf5974d9785f39802363e2997df91a8c
Dev-Iyke <iyke.thompson01@gmail.com>
2025-11-14 14:43:34 +0100

    Wrapped app in src folder

---
+++ src/app/(global)/about/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div className='font-garantpro font-semibold'>AboutPage
+      <Button>Game time</Button>
+    </div>
+  )
+}
+
+export default AboutPage
